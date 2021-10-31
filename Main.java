cc674df60cf384ac54efb10d10acf1518dd18c36
Drake1601 <83598867+Drake1601@users.noreply.github.com>
2021-10-31 21:18:33 +0700

    upload

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 
@@ -12,7 +13,7 @@ public class Main extends Application {
 		try {
 			primaryStage.setTitle("Display");
             
-			BorderPane root = new BorderPane();
+			AnchorPane root = new AnchorPane();
 			root.setBackground(null);
 			Scene scene = new Scene(root,460,460);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
